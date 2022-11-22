a94d956f273798be9520744f233d1a1a3c4b2f34
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-22 11:00:32 -0800

    added base model test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,3 +7,12 @@ import pep8
 from datetime import datetime
 import inspect
 import json
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                "Found code style errors (and warnings).")
