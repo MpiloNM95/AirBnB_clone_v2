96aa2c1b1827c3e53ad3349fd14c6d3c28acfd8d
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-22 13:14:19 -0800

    added permissions

--- models/base_model.py
+++ models/base_model.py
@@ -6,3 +6,10 @@ from datetime import datetime
 import json
 from uuid import uuid4
 import models
+
+
+class BaseModel:
+     """Class that defines all common attr/methods for other classes"""
+
+     def __init__(self, *args, **kwargs):
+         """Initialization of Class"""
