d7c96b861d5c5c7d075becf422f75cedb86542b5
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-22 07:47:32 -0800

    added base model test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Test for BaseModel class"""
+
+import unittest
+from models.base_model import BaseModel
+import pep8
+from datetime import datetime
+import inspect
+import json
