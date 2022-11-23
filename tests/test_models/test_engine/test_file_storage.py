6155a526a22c406c85b0504e8aa345ad3053fd4b
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-23 04:34:14 -0800

    added new test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,3 +6,8 @@ from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.engine.file_storage import FileStorage
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.user import User
+from os import path
