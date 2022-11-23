4ac54eac1a339fb20f8930fabdc9a772ae0cf822
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-23 04:14:12 -0800

    added new test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,14 +2,3 @@
 """test the FileStorage method"""
 import json
 from models import storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.engine.file_storage import FileStorage
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.user import User
-from os import path
-import pep8
-import unittest
