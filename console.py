d6fe0ae83dfb68dc810dbd6d31af81d476e89601
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-24 09:26:02 -0800

    added to console

--- console.py
+++ console.py
@@ -4,3 +4,16 @@ import cmd
 import json
 from models.base_model import BaseModel
 from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    ALX Africa AirBnB clone console
+    contains the entry point of the comm interpreter
+    quit nd EOF to exit the program
+    help displays help
+    an empty line + ENTER doesn't execute anything
+    """
+    prompt = "(hbnb) "
+    attributes = ["updated_at", "created_at", "id"]
+    specs = ["\'", "\""]
