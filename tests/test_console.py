2484b17bcf0bc73a0ce2b489398df7d26f1037f6
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-24 03:32:10 -0800

    added console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,3 +9,13 @@ import pep8
 from unittest.mock import patch
 from io import StringIO
 import cmd
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                "Found code style errors (and warnings).")
