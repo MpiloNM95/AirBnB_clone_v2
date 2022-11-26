f9003976e760de5aa9fde3f279a42fd2c281e55f
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-26 04:50:37 -0800

    added city import to file storage test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 import json
 from models import storage
 from models.base_model import BaseModel
+from models.city import City
 from models.engine.file_storage import FileStorage
 from models.state import State
 from models.user import User
