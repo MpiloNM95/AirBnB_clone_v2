a8e092f4220d008c63ddbb0354748f9bfc40876a
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-26 11:35:56 -0800

    added place to console

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
+from models.place import Place
 from model.state import State
 from models.user import User
 from models import storage
@@ -19,7 +20,8 @@ class HBNBCommand(cmd.Cmd):
     an empty line + ENTER doesn't execute anything
     """
     prompt = "(hbnb) "
-    classes = ["BaseModel", "User", "State", "City", "Amenity"]
+    classes = ["BaseModel", "User", "State", "City", 
+               "Amenity", "Place"]
     attributes = ["updated_at", "created_at", "id"]
     specs = ["\'", "\""]
 
