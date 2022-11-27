a8a68bd346e78c2698b90fba51d168e0f91d3018
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-27 06:03:43 -0800

    fixed and update console and console test

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-'''This file makes the models folder a python package'''
-
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-
-storage = FileStorage()
-storage.reload()
--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''The Amenity class that inherits from the BaseModel'''
-
-from models.base_model import BaseModel
-
-class Amenity(BaseModel):
-    '''This class takes care of all application amenities'''
-    name = ''
-    count = 0
