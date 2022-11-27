dbc5d61ee574bc98c61d3345aa5bd91955751704
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-27 01:55:23 -0800

    added review test

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -15,3 +15,12 @@ class Test_pep8(unittest.TestCase):
         result = pep8style.check_files(['models/review.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+
+class TestDocs(unittest.TestCase):
+    """Base model document tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Testing class"""
+        cls.review_funcs = inspect.getmembers(Review, inspect.isfunction)
