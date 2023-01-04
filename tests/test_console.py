5492350566bd4fd40d2063023ee050fbbfb65e36
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-04 05:13:02 -0800

    added pep8 to test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,4 +17,11 @@ from os import getenv, pathconf
 from console import HBNBCommand
 
 
-
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                "Found code style errors (and warnings).")
