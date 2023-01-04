712152a7c633bd43a2f177201e1539e39abb0613
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-04 07:09:38 -0800

    added console

--- console.py
+++ console.py
@@ -35,7 +35,15 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Ignore empty spaces."""
         pass
+    
+    def do_quit(self, line):
+        """Quit command to exit the program."""
+        return True
 
+    def do_EOF(self, line):
+        """EOF signal to exit the program."""
+        print("")
+        return True
 
 
 if __name__ == '__main__':
