4fe14866c44c585d491d47e74c3b173f0a0c9f0b
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-04 05:18:06 -0800

    added test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -32,3 +32,10 @@ class Test_pep8(unittest.TestCase):
                 "console.py needs a docstring")
         self.assertTrue(len(__import__("console").__doc__) >= 1,
                 "console.py needs a docstring")
+
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                "HBNBCommand class needs a docstring")
