5b7229b6962f39d30a99a850391d2c5802457aec
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-04 07:57:42 -0800

    added create to console

--- console.py
+++ console.py
@@ -45,6 +45,42 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
+    def do_create(self, line):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create  new class instance with given keys/values and prints its id. 
+        """
+
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[O] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                    kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[O])()
+            else:
+                obj = eval(my_list[O])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
