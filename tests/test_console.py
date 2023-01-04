d44572c012c2cf9838136ea1522cb8189436d9d2
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-04 05:21:04 -0800

    added test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,3 +39,14 @@ class Test_pep8(unittest.TestCase):
                 "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                 "HBNBCommand class needs a docstring")
+
+
+class test_console(unittest.TestCase):
+    """Tests the console module."""
+
+    def test_create(self):
+        """tests the create method"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("create")
+            self.assertEqual(
+                    "** class name missing **\n", f.getvalue())
