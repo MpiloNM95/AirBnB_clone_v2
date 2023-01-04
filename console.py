32f6b70ac30e89982f5d1d6d3f8709e4486b3a02
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-04 09:12:28 -0800

    added show to console

--- console.py
+++ console.py
@@ -58,7 +58,7 @@ class HBNBCommand(cmd.Cmd):
             kwargs = {}
             for i in range(1, len(my_list)):
                 key, value = tuple(my_list[i].split("="))
-                if value[O] == '"':
+                if value[0] == '"':
                     value = value.strip('"').replace("_", " ")
                 else:
                     try:
@@ -68,9 +68,9 @@ class HBNBCommand(cmd.Cmd):
                     kwargs[key] = value
 
             if kwargs == {}:
-                obj = eval(my_list[O])()
+                obj = eval(my_list[0])()
             else:
-                obj = eval(my_list[O])(**kwargs)
+                obj = eval(my_list[0])(**kwargs)
                 storage.new(obj)
             print(obj.id)
             obj.save()
@@ -80,7 +80,36 @@ class HBNBCommand(cmd.Cmd):
         except NameError:
             print("** class doesn't exist **")
 
-
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        Exceptions:
+            SyntaxError: when there is no args given
+            NameError: when there is no object that has the name
+            IndexError: when there  is no id given
+            KeyError: where ther is no valid id given
+        """
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+            if my_list[0] not in self.__classes:
+                raise NameError()
+            if len(my_list) <2:
+                raise IndexError()
+            objects = storage.all()
+            key = my_list[0] + '.' + my_list[1]
+            if key in objects:
+                print(objects[key])
+            else:
+                raise KeyError()
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+        except IndexError:
+            print("** instance id missing **")
+        except KeyError:
+            print("** no instance found **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
