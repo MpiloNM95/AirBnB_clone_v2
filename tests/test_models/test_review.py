fc065da191bdaa55d2606430c10fc9d4f5249774
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-06 06:57:11 -0800

    added __init__.py to review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""test of review module"""
-import unittest
+"""test of review class"""
 from models.base_model import BaseModel
 from models.review import Review
 import pep8
-import inspect
 
 
 class Test_pep8(unittest.TestCase):
@@ -17,29 +15,11 @@ class Test_pep8(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
 
-class TestDocs(unittest.TestCase):
-    """Base model document tests"""
+class test_review(BaseModel):
+    """ tests the review class """
 
-    @classmethod
-    def setUpClass(cls):
-        """Testing class"""
-        cls.review_funcs = inspect.getmembers(Review, inspect.isfunction)
-
-    def test_module_docstring(self):
-        """module docstring length"""
-        self.assertTrue(len(Review.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """Class docstring length"""
-        self.assertTrue(len(Review.__doc__) >= 1)
-
-
-class TestReview(unittest.TestCase):
-        """Test Review Class"""
-
-        def test_review(self):
-            """test the reviewclass"""
-            self.assertEqual(Review.place_id, "")
-            self.assertEqual(Review.user_id, "")
-            self.assertEqual(Review.text, "")
-            self.assertTrue(issubclass(Review, Review))
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
